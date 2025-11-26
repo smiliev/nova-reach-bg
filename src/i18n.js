b230027e5be76856ad0b3a564ea6186f54e4e8e9
novareach2025 <novareach2025@gmail.com>
2025-11-26 10:59:09 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ const resources = {
       nav: {
         home: "Начало",
         about: "За нас",
-        services: "Нашите услуги",
+        services: "услуги",
         portfolio: "Портфолио",
         contact: "Контакти",
         contactUs: "Свържи се с нас"
